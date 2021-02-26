57ba5f487834a8e329b9a0dc6e5133d098c8963a
nickmly <nickmly@gmail.com>
2021-02-25 21:46:54 -0500

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -9,17 +10,14 @@ const connection = mysql.createConnection({
   database: process.env.DB_DB
 });
 
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-
-  connection.connect();
-
-  connection.query('SELECT * FROM exercises', function (error, results, fields) {
+  connection.query('SELECT * FROM exercises LIMIT 1', function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });
-
-  connection.end();
-  
 });
 
 app.listen(8000, () => {
