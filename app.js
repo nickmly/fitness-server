f32b753c3390c250d17f3c8f2a28ffecaad07398
nickmly <nickmly@gmail.com>
2021-02-28 12:53:23 -0500

    Delete workout and sets from workout

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.use(cors());
 app.get('/exercises', (req, res) => {
   db.query('SELECT * FROM exercises', function (error, results, fields) {
     if (error) throw error;
-    res.json(results);
+    res.status(200).json(results);
   });
 });
 
 app.get('/log', (req, res) => {
   db.query(`SELECT * FROM logs WHERE user_id='${req.query.uid}' AND date='${req.query.date}'`, function (error, results, fields) {
     if (error) throw error;
-    res.json(results);
+    res.status(200).json(results);
   });
 });
 
@@ -33,7 +33,7 @@ app.post('/log', (req, res) => {
   const id = uuidv4();
   db.query(`INSERT INTO logs(id, date, user_id) VALUES('${id}', '${req.body.date}', '${req.body.uid}')`, function (error, results, fields) {
     if (error) throw error;
-    res.send(id);
+    res.status(200).send(id);
   });
 });
 
@@ -41,7 +41,7 @@ app.post('/log', (req, res) => {
 app.get('/workout', (req, res) => {
   db.query(`SELECT * FROM workouts WHERE log_id='${req.query.log_id}'`, function (error, results, fields) {
     if (error) throw error;
-    res.send(results);
+    res.status(200).send(results);
   });
 });
 
@@ -49,7 +49,14 @@ app.post('/workout', (req, res) => {
   const id = uuidv4();
   db.query(`INSERT INTO workouts(id, log_id, exercise_id) VALUES('${id}', '${req.body.log_id}', '${req.body.exercise_id}')`, function (error, results, fields) {
     if (error) throw error;
-    res.send(id);
+    res.status(200).send(id);
+  });
+});
+
+app.delete('/workout', (req, res) => {
+  db.query(`DELETE FROM workouts WHERE id='${req.query.workout_id}'`, function (error, results, fields) {
+    if (error) throw error;
+    res.status(200).send("Success");
   });
 });
 
@@ -57,7 +64,7 @@ app.post('/workout', (req, res) => {
 app.get('/sets', (req, res) => {
   db.query(`SELECT * FROM sets WHERE workout_id='${req.query.workout_id}'`, function (error, results, fields) {
     if (error) throw error;
-    res.send(results);
+    res.status(200).send(results);
   });
 });
 
@@ -65,12 +72,22 @@ app.post('/set', (req, res) => {
   const id = uuidv4();
   db.query(`INSERT INTO sets(id, workout_id, reps, weight) VALUES('${id}', '${req.body.workout_id}', '${req.body.reps}', '${req.body.weight}')`, function (error, results, fields) {
     if (error) throw error;
-    res.send(id);
+    res.status(200).send(id);
   });
 });
 
 app.delete('/set', (req, res) => {
   db.query(`DELETE FROM sets WHERE id='${req.query.id}'`, function (error, results, fields) {
+    if (error) {
+      res.status(500).send(error);
+      throw error;
+    } 
+    res.status(200).send("Success");
+  });
+});
+
+app.delete('/sets', (req, res) => {
+  db.query(`DELETE FROM sets WHERE workout_id='${req.query.workout_id}'`, function (error, results, fields) {
     if (error) {
       res.status(500).send(error);
       throw error;
