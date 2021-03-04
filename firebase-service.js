d6854e5d6b92072c78d50de4b0e8bb6611537d2d
nickmly <nickmly@gmail.com>
2021-03-04 16:24:36 -0500

    Use env file for firebase creds

--- firebase-service.js
+++ firebase-service.js
@@ -1,7 +1,11 @@
 const admin = require("firebase-admin");
 
+const key = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": key,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
-
 module.exports = admin;
